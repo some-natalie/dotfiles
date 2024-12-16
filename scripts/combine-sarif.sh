f254cdaa59c9d41502550d605699ed8267c366e5
Natalie Somersall <natalie.somersall@gmail.com>
2024-12-16 09:57:13 -0700

    shellcheck clean up

--- scripts/combine-sarif.sh
+++ scripts/combine-sarif.sh
@@ -22,25 +22,40 @@ function get_results() {
 function merge_sarif() {
   local file1="$1"
   local file2="$2"
-  local j1=$(cat "$file1")
-  local j2=$(cat "$file2")
 
-  local rules1=$(echo "$j1" | jq '.runs[0].tool.driver.rules')
-  local rules2=$(echo "$j2" | jq '.runs[0].tool.driver.rules')
-  local results1=$(echo "$j1" | jq '.runs[0].results')
-  local results2=$(echo "$j2" | jq '.runs[0].results')
+  local j1
+  j1=$(cat "$file1")
+
+  local j2
+  j2=$(cat "$file2")
+
+  local rules1
+  rules1=$(echo "$j1" | jq '.runs[0].tool.driver.rules')
+
+  local rules2
+  rules2=$(echo "$j2" | jq '.runs[0].tool.driver.rules')
+
+  local results1
+  results1=$(echo "$j1" | jq '.runs[0].results')
+
+  local results2
+  results2=$(echo "$j2" | jq '.runs[0].results')
 
   echo "$mergeInto rules found: $(echo "$rules1" | jq 'length')"
   echo "$mergeInto locations found: $(echo "$results1" | jq 'length')"
   echo "$mergeFrom rules found: $(echo "$rules2" | jq 'length')"
   echo "$mergeFrom locations found: $(echo "$results2" | jq 'length')"
 
-  local combined_rules=$(jq -s '.[0] + .[1]' <(echo "$rules1") <(echo "$rules2"))
-  local unique_rules=$(echo "$combined_rules" | jq 'unique_by(.id)')
+  local combined_rules
+  combined_rules=$(jq -s '.[0] + .[1]' <(echo "$rules1") <(echo "$rules2"))
+
+  local unique_rules
+  unique_rules=$(echo "$combined_rules" | jq 'unique_by(.id)')
 
   echo "Number of rules combined is: $(echo "$unique_rules" | jq 'length')"
 
-  local combined_results=$(jq -s '.[0] + .[1]' <(echo "$results1") <(echo "$results2"))
+  local combined_results
+  combined_results=$(jq -s '.[0] + .[1]' <(echo "$results1") <(echo "$results2"))
 
   jq '.runs[0].tool.driver.rules = '"$unique_rules"' | .runs[0].results = '"$combined_results" "$file1" > all_results.sarif
 
